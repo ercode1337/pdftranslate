a087dc7975e44b07ce1c5ecbf9da047f18995ebd
ercode1337 <eric.melin@outlook.com>
2023-03-30 01:31:25 +0300

    Some changes made to js

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
+// Talkano - coded by ercode1337
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Handler for "Try Talkano" button click
+  document.getElementById("conBtn").addEventListener("click", function() {
+    // Replace current HTML content with "Talkano is coming soon!"
+    document.getElementById("conP").innerHTML = "Talkano is coming soon!";
+  });
+});
+
+
+// Get references to the input and output boxes
 const inputBox = document.getElementById('tagBox');
 const outputBox = document.getElementById('outputBox');
-let tagCount = 0;
-const tagsArray = []; // declare an empty array to store the tags
 
+// Load existing tags from local storage, or start with an empty array if none exist
+let tagCount = 0;
+let tagsArray = JSON.parse(localStorage.getItem('tags')) || [];
+
+// Function to update local storage with the current state of the tags array
+function updateTagsArray() {
+  localStorage.setItem('tags', JSON.stringify(tagsArray));
+}
+
+// Function to remove a tag from both the output box and the tags array
+function removeTag(tagBox) {
+  const tagIndex = tagsArray.indexOf(tagBox.innerText);
+  tagsArray.splice(tagIndex, 1);
+  updateTagsArray();
+  outputBox.removeChild(tagBox);
+  tagCount--;
+}
+
+// Event listener for the input box, to add tags when the user presses enter
 inputBox.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
-    const value = inputBox.value.trim(); // trim extra white spaces from the beginning and end of the input
-    const regex = /^[a-zA-Z\s]*$/; // use regex to check if the input contains only letters and spaces
-    if (value.length > 0 && regex.test(value) && tagCount < 10) {
-      const tagBox = document.createElement('div');
-      tagBox.innerText = value;
-      outputBox.appendChild(tagBox);
-      inputBox.value = '';
-      tagsArray.push(value); // add the new tag's value to the tagsArray
-      tagCount++;
+    const value = inputBox.value.trim(); // Get the trimmed input value
+    const regex = /^[a-zA-Z\s]*$/; // Define a regular expression to check if the value contains only letters and spaces
+    if (value.length > 0 && regex.test(value) && tagCount < 5) { // If the value is not empty, contains only letters and spaces, and there are fewer than 5 tags already
+      const tagBox = document.createElement('div'); // Create a new tag element
+      tagBox.innerText = value; // Set the tag element's text to the input value
+      outputBox.appendChild(tagBox); // Add the tag element to the output box
+      inputBox.value = ''; // Reset the input box value to an empty string
+      tagsArray.push(value); // Add the tag value to the tags array
+      updateTagsArray(); // Update local storage with the current state of the tags array
+      tagCount++; // Increment the tag count
     } else {
-      return;
+      return; // Otherwise, do nothing
     }
+  }
+});
+
+// Event listener for the output box, to remove tags when the user clicks on them
+outputBox.addEventListener('click', e => {
+  if (e.target.tagName === 'DIV') { // If the user clicked on a tag element
+    removeTag(e.target); // Remove the corresponding tag from the output box and the tags array
+  }
+});
+
+// On page load, add any existing tags from local storage to the output box
+window.addEventListener('load', () => {
+  if (tagsArray.length > 0) {
+    tagsArray.forEach(tag => {
+      const tagBox = document.createElement('div');
+      tagBox.innerText = tag;
+      outputBox.appendChild(tagBox);
+      tagCount++;
+    });
   }
 });
